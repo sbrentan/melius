9d78fd2637f54afb22b3bd92698b0148be8bba9f
Simone <simobrentan@gmail.com>
2022-05-15 19:07:29 +0200

    Added books Api

--- app/controllers/ui/books.js
+++ app/controllers/ui/books.js
@@ -1,4 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../../config')
+const Book = require("../../models/book")
+
+router.get("/", async function(req, res) {
+    Book.find({}, function(err, result) {
+        res.render('books', {books: result});
+    })
+})
+
+router.get('/new', async function(req, res) {
+    res.render('book_edit', {book: false});
+})
+
+router.get('/:id', async function(req, res) {
+    Book.findOne({_id: req.params.id}, async function(err, result) {
+        if(err)
+            res.status(404).send("Book not found")
+        else
+            console.log(result)
+            res.render('book_edit', {book: result});
+    })
+})
 
 module.exports = router
