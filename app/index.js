456cfd9ad6eeac0a8b08e3b3da2d42097c5cb6e7
Matteo <matteo68.mc@gmail.com>
2022-05-22 16:43:16 +0200

    Added homepage, created cloud MongoDB database on atlas, added some css

--- app/index.js
+++ app/index.js
@@ -4,27 +4,27 @@ const app = require('./app.js');
 var mongoose = require('mongoose');
 
 //Set up default mongoose connection
-var mongoDB = 'mongodb://localhost:27017/melius';
+//var mongoDB = 'mongodb://localhost:27017/melius';
+var mongoDB = 'mongodb+srv://melius:qwerty123@melius.i26zq.mongodb.net/?retryWrites=true&w=majority';
 
 const port = process.env.PORT || 80;
-
 
 //Get the default connection
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
 .then ( () => {
-    
+
     console.log("Connected to Database");
-    
+
     app.listen(port, () => {
         console.log(`Server listening on port ${port}`);
     });
-    
+
 });
 
 var db = mongoose.connection
 
 db.on('error', console.error.bind(console, 'connection error:'));
- 
+
 db.once('open', function() {
     console.log("Connection Successful!");
 });
